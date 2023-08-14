19299bb56cb5f5581b87e8827d9731a1b30ec208
Tantannn <tango.11@yahoo.com>
2023-08-14 21:09:59 +0700

    update

--- app.js
+++ app.js
@@ -14,23 +14,69 @@ let bottom = 0;
 
 const Graphics = PIXI.Graphics;
 
-/* class Mario {
-  constructor(position, velocity, texture) {
-    super(texture);
+class Rect {
+  constructor({ position, size }) {
     this.position = position;
-    this.velocity = velocity;
+    this.size = size;
   }
   draw() {
-    this.beginFill(0xff0000);
-    this.drawRect(0, 0, 50, 50);
-    this.endFill();
+    this.shape = new Graphics();
+    this.shape
+      .beginFill(0xff0000)
+      .drawRect(this.position.x, this.position.y, this.size.x, this.size.y)
+      .endFill();
+    app.stage.addChild(this.shape);
+  }
+  hit() {
+    if (rectsIntersectPlatform(mario, this.shape)) {
+       (mario.y -= 10);
+    }
   }
 }
-const mario1 = new Mario(0, 1);
 
-// app.stage.addChild(mario1);
+class Coin {
+  constructor({ position, size }) {
+    this.position = position;
+    this.size = size;
+    this.color = 0xffff00;
+  }
+  draw() {
+    this.shape = new Graphics();
+    this.shape
+      .beginFill(0xffff00)
+      .drawCircle(this.position.x, this.position.y, this.size)
+      .endFill();
+    app.stage.addChild(this.shape);
+  }
+  hit() {
+    if (rectsIntersectCoin(mario, this.shape)) {
+     app.stage.removeChild(this.shape);
+    }
+  }
+}
 
-console.log(PIXI.Sprite, Graphics); */
+const rectangle1 = new Rect({
+  position: { x: 300, y: 375 },
+  size: { x: 100, y: 50 },
+});
+const rectangle2 = new Rect({
+  position: { x: 500, y: 300 },
+  size: { x: 100, y: 50 },
+});
+const rectangle3 = new Rect({
+  position: { x: 100, y: 450 },
+  size: { x: 100, y: 50 },
+});
+rectangle1.draw();
+rectangle2.draw();
+rectangle3.draw();
+
+const coin1 = new Coin({position: {x: 150, y: 425}, size: 25});
+const coin2 = new Coin({position: {x: 350, y: 350}, size: 25});
+const coin3 = new Coin({position: {x: 550, y: 275}, size: 25});
+coin1.draw();
+coin2.draw();
+coin3.draw();
 
 const mario = new Graphics();
 mario
@@ -46,40 +92,9 @@ floor
   .drawRect(0, 0, 5000, 50)
   .endFill();
 
-const rectangle2 = new Graphics();
-rectangle2
-  .beginFill(0xff0000)
-  .lineStyle(2, 0xaa33bb)
-  .drawRect(500, 300, 100, 50)
-  .endFill();
-const rectangle1 = new Graphics();
-rectangle1
-  .beginFill(0xff0000)
-  .lineStyle(2, 0xaa33bb)
-  .drawRect(300, 375, 100, 50)
-  .endFill();
-const rectangle3 = new Graphics();
-rectangle3
-  .beginFill(0xff0000)
-  .lineStyle(2, 0xaa33bb)
-  .drawRect(100, 450, 100, 50)
-  .endFill();
-
-const coin1 = new Graphics();
-coin1.beginFill(0xffff00).drawCircle(150, 425, 25).endFill();
-const coin2 = new Graphics();
-coin2.beginFill(0xffff00).drawCircle(350, 350, 25).endFill();
-const coin3 = new Graphics();
-coin3.beginFill(0xffff00).drawCircle(550, 275, 25).endFill();
 app.stage.addChild(
   mario,
-  rectangle2,
   floor,
-  rectangle1,
-  rectangle3,
-  coin1,
-  coin2,
-  coin3,
 );
 
 mario.position.set(0, 0);
@@ -108,10 +123,8 @@ spaceShipSprite.position.set(100, 50);
 spaceShipSprite.scale.x = 0.5;
 spaceShipSprite.scale.y = 0.5;
 spaceShipSprite.rotation = 1.5;
-console.log(app.ticker.update());
-app.ticker.add((deltaMS) => {
-  /* text.x = Math.random() * app.screen.width;
-  text.y = Math.random() * app.screen.height; */
+
+app.ticker.add((delta) => {
   spaceShipSprite.x += 1;
   if (circle.position.y < floor.position.y / 2) {
     circle.y += 2;
@@ -119,45 +132,31 @@ app.ticker.add((deltaMS) => {
   if (mario.position.y + 50 < floor.position.y) {
     mario.y += 10;
   }
-  // console.log(keys);
   if (keys.d) {
     mario.x += 5;
   }
   if (keys.a) {
     mario.x -= 5;
   }
-  if (keys.w) { mario.y -= 20
-const time = setTimeout(() => {keys.w = false
-console.log(keys)
+  if (keys.w) {
+    mario.y -= 20;
+    const time = setTimeout(() => {
+      keys.w = false;
     }, 230);
   }
-  if (rectsIntersectPlatform(mario, rectangle2)) {
-    mario.y -= 10;
-  }
-  if (rectsIntersectPlatform(mario, rectangle1)) {
-    mario.y -= 10;
-  }
-  if (rectsIntersectPlatform(mario, rectangle3)) {
-    mario.y -= 10;
-  }
-  if (rectsIntersectCoin(mario, coin1)) {
-    app.stage.removeChild(coin1);
-  }
-  if (rectsIntersectCoin(mario, coin2)) {
-    app.stage.removeChild(coin2);
-  }
-  if (rectsIntersectCoin(mario, coin3)) {
-    app.stage.removeChild(coin3);
-  }
+  rectangle1.hit();
+  rectangle2.hit();
+  rectangle3.hit();
+  coin1.hit();
+  coin2.hit();
+  coin3.hit();
+
   score = app.stage.children.length;
   updateScore(11 - score);
 });
 
 mario.buttonMode = true;
 mario.interactive = true;
-/* rectangle.on("pointerdown", () => {
-    rectangle.x += 0.1
-}) */
 
 let keys = {};
 document.addEventListener("keydown", (e) => {
@@ -188,13 +187,7 @@ const rectsIntersectCoin = (a, b) => {
     aBox.y < bBox.y + bBox.height
   );
 };
-/* const rectPlatform = (a, b) => {
-  let aBox = a.getBounds();
-  let bBox = b.getBounds();
-  return (
-    aBox.x + aBox.width > bBox.x 
-  );
-}; */
+
 
 const updateScore = (score) => {
   text.text = `Score: ${score}`;
@@ -206,12 +199,8 @@ const updateScore = (score) => {
   console.log( mario.y, bottom);
 }; */
 
-
-
 const jump = () => {
-
-const time = setTimeout(() => { mario.y -= 20}, 50);
-  
-
-
-}
+  const time = setTimeout(() => {
+    mario.y -= 20;
+  }, 50);
+};
